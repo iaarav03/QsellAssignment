f55bcc06a535c4aed5bdd51c13995e3136c24742
iaarav03 <aarav.21ug2059@iiitranchi.ac.in>
2024-10-15 01:42:40 +0530

    newchanges

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,27 +1,24 @@
 import React from "react";
 import "./Card.css";
-import my_pic from "../my_pic.jpg"
-import { 
-  BacklogIcon, 
-  TodoIcon, 
-  InProgressIcon, 
-  DoneIcon, 
+import my_pic from "../my_pic.jpg";
+import {
+  BacklogIcon,
+  TodoIcon,
+  InProgressIcon,
+  DoneIcon,
   DefaultStatusIcon,
   LowPriorityIcon,
   MediumPriorityIcon,
   HighPriorityIcon,
   UrgentPriorityIcon2,
   NoPriorityIcon,
-  
-
-}  from "../Icons";
-const Card = ({ id, title, tag, status, priority,type }) => {
-
+} from "../Icons";
+const Card = ({ id, title, tag, status, priority, type }) => {
   const isStatus = type === "status";
   const isPriority = type === "priority";
-  const isUser=type==="user";
+  const isUser = type === "user";
 
-  const statusOrder = ['Backlog', 'Todo', 'In progress', 'Done'];
+  const statusOrder = ["Backlog", "Todo", "In progress", "Done"];
   const getStatusIndex = (status) => {
     return statusOrder.indexOf(status);
   };
@@ -31,72 +28,60 @@ const Card = ({ id, title, tag, status, priority,type }) => {
         <span style={{ textTransform: "uppercase" }} className="color-grey">
           {id}
         </span>
-        {isUser===false && (<div
-          className="imageContainer relative"
-          style={{ width: "30px", height: "30px" }}
-        >
-           
-           <img
-                        src={my_pic}
-                        style={{ width: "25px", borderRadius: "50%" }}
-                      />
-          <div className="showStatus"></div>
-        </div>)}
-        
+        {isUser === false && (
+          <div
+            className="imageContainer relative"
+            style={{ width: "30px", height: "30px" }}
+          >
+            <img src={my_pic} style={{ width: "25px", borderRadius: "50%" }} />
+            <div className="showStatus"></div>
+          </div>
+        )}
       </div>
       <div className="cardTitle" style={{ fontWeight: 200 }}>
         {!isStatus &&
-          (
-            status === "Backlog" ? (
-              <BacklogIcon/>
-            )
-              : status === "Todo" ? (
-               <TodoIcon/>
-              ) : status === "In progress" ? (
-                <InProgressIcon/>
-              ) : status === "Done" ? (
-               <DoneIcon/>
-              )
-                : (
-                 <DefaultStatusIcon/>
-                ))}
-        <span style={{ margin: "0.2px", paddingLeft:"5px" }}>{title}</span>
+          (status === "Backlog" ? (
+            <BacklogIcon />
+          ) : status === "Todo" ? (
+            <TodoIcon />
+          ) : status === "In progress" ? (
+            <InProgressIcon />
+          ) : status === "Done" ? (
+            <DoneIcon />
+          ) : (
+            <DefaultStatusIcon />
+          ))}
+        <span style={{ margin: "0.2px", paddingLeft: "5px" }}>{title}</span>
       </div>
       <div className="cardTags">
         {!isPriority ? (
           <div className="tags color-grey">
-            
-            
-               {priority === 1 && (
-                   <LowPriorityIcon/>
-                    
-                  )}
-                  {priority === 2 && (
-                   <MediumPriorityIcon/>
-                    
-                  )}
-                  {priority === 3 && (
-                   <HighPriorityIcon/>
-                    
-                  )}
-                  {priority === 4 && (
-                    <UrgentPriorityIcon2/>
-                  )
-
-}
-{priority === 0 && 
-  <NoPriorityIcon/>
-
-}
-             
+            {priority === 1 && <LowPriorityIcon />}
+            {priority === 2 && <MediumPriorityIcon />}
+            {priority === 3 && <HighPriorityIcon />}
+            {priority === 4 && <UrgentPriorityIcon2 />}
+            {priority === 0 && <NoPriorityIcon />}
           </div>
         ) : null}
         {tag?.map((element, index) => {
           return (
-            <div key={index} className="tags color-grey">
-              {" "}
-              <span>•</span> {element}
-            </div>
+            <>
+              <div key={index} className="tags color-grey" style={{}}>
+                <span
+                  style={{
+                    display: "inline-block",
+                    width: "8px",
+                    height: "8px",
+                    backgroundColor: "grey",
+                    borderRadius: "50%",
+                    marginLeft: "4px",
+                  }}
+                ></span>
+                {"  "}
+
+                {element}
+              </div>
+            </>
           );
         })}
       </div>
